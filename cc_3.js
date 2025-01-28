50bbbfcd3ffe93696db93fe3f6588f511155c34f
ssj2-z <ssj2@usf.edu>
2025-01-28 13:46:16 -0500

    "Task 1 - Product Price Management."

--- cc_3.js
+++ cc_3.js
@@ -1,6 +1,6 @@
 //Task 1: Declare array with at least five numerical vallues
 
-let price = [20, 30, 40, 50, 60];
+let price = [20, 30, 40, 50, 55, 60];
 price.push(70);
 prices.shift();
 console.log(prices);
