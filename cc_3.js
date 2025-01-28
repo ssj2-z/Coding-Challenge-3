306bc2279b75222db0361f44a2206fb20459273f
ssj2-z <ssj2@usf.edu>
2025-01-28 13:56:06 -0500

    "Task 2 - Modifying Customer Orders."

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,12 @@ let price = [20, 30, 40, 50, 55, 60];
 price.push(70);
 prices.shift();
 console.log(prices);
+
+//Task 2: Modifying Customer Orders
+
+let orders = [5, 10, 15, 20, 25];
+orders[2] +=5; // The third element is at index 2
+let totalOrders = orders.reduce((total, order) => total + order, 0);
+console.log('Update orders:', orders);
+console.log('Total number of orders:' totalOrders);
+
